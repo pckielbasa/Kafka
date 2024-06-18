04100d1184ea98aa39ec165a2b693802f3377b3e
pckielbasa <pckielbasa@gmail.com>
2024-06-18 22:02:57 +0200

    Add kafka producer configuration

--- order-service/src/main/java/org/orderservice/web/OrderController.java
+++ order-service/src/main/java/org/orderservice/web/OrderController.java
@@ -2,8 +2,8 @@ package org.orderservice.web;
 
 import lombok.RequiredArgsConstructor;
 import org.orderservice.application.OrderService;
+import org.orderservice.infrastructure.kafka.OrderProducer;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+    private final OrderProducer orderProducer;
+
 
     @PostMapping(path = "/add")
     void addOrder() {
-        orderService.addOrder();
+        orderProducer.sendMessageToKafka("order-service","Test","test");
     }
 }
