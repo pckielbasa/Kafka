7d92f923b2aa2e374788360c561ba7df19bca26a
pckielbasa <pckielbasa@gmail.com>
2024-06-27 16:31:05 +0200

    Add Payment kafka configuration

---
+++ payment-service/src/main/java/org/paymentservice/PaymentConsumer.java
@@ -0,0 +1,13 @@
+package org.paymentservice;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentConsumer {
+
+    @KafkaListener(topics = "order-service", groupId = "payment-service")
+    public void listen(String message) {
+        System.out.println("Received Message in group foo: " + message);
+    }
+}
